7701a4bfc8add217dda36e12c4f56fdcacc2f105
Damien AICHEH <damien.aicheh@gmail.com>
2021-05-14 22:38:57 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Update Android version Manifest action
+
+This action update the `versionCode` and `versionName` properties of the AndroidManifest.xml file for your Android projects.
+
+## Inputs
+
+### `android-manifest-path`
+
+**Required** The relative path for the AndroidManifest.xml file.
+
+### `version-code` 
+  
+**Required** The value of the version code, this must be between 0 and 2100000000.
+
+###  `version-name`
+    
+**Required** The value of the version name
+
+###  `print-file`
+
+Output the AndroidManifest.xml file in console before and after update.
+
+## Usage
+
+```yaml
+- name: Update AndroidManifest.xml
+  uses: damienaicheh/update-android-version-manifest-action@v1.0.0
+  with:
+    android-manifest-path: './path_to_your/AndroidManifest.xml'
+    version-code: 2
+    version-name: '2.0'
+    print-file: true
+```
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as fs from 'fs';
+
+process.on('unhandledRejection', handleError)
+main().catch(handleError)
+
+async function main(): Promise<void> {
+    try {
+        let printFile = getBooleanInput('print-file');
+        let androidManifestPath = core.getInput('android-manifest-path');
+
+        if (!fs.existsSync(androidManifestPath)) {
+            core.setFailed(`The file path for the AndroidManifest.xml does not exist or is not found: ${androidManifestPath}`);
+            process.exit(1);
+        }
+
+        let versionCode = core.getInput('version-code');
+
+        if (!versionCode) {
+            core.setFailed(`Version Code has no value: ${versionCode}`);
+            process.exit(1);
+        } else if (parseInt(versionCode, 0) <= 0 && parseInt(versionCode, 10) >= 2100000000) {
+            core.setFailed(`The Version Code you set: ${versionCode} is not valid, to submit your application to the Google Play Store the value must 
+            be greater then 0 and below 2100000000 of ${versionCode}`);
+            process.exit(1);
+        }
+
+        let versionName: string = core.getInput('version-name');
+
+        if (printFile) {
+            core.info('Before update:');
+            await exec.exec('cat', [androidManifestPath]);
+        }
+
+        let filecontent = fs.readFileSync(androidManifestPath).toString();
+        fs.chmodSync(androidManifestPath, "600");
+
+        filecontent = filecontent.replace(/versionCode\s*=\s*"(\d+(?:\.\d)*)"/mg, `versionCode=\"${versionCode}\"`);
+        filecontent = filecontent.replace(/versionName\s*=\s*"(\d+(?:\.\d+)*)"/mg, `versionName=\"${versionName}\"`);
+
+        fs.writeFileSync(androidManifestPath, filecontent);
+
+        if (printFile) {
+            core.info('After update:');
+            await exec.exec('cat', [androidManifestPath]);
+        }
+
+        core.info(`AndroidManifest.xml updated successfully with versionCode: ${versionCode} and versionName: ${versionName}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+function handleError(err: any): void {
+    console.error(err)
+    core.setFailed(`Unhandled error: ${err}`)
+}
+
+function getBooleanInput(inputName: string, defaultValue: boolean = false): boolean {
+    return (core.getInput(inputName) || String(defaultValue)).toUpperCase() === 'TRUE';
+}
